442c386eb1a78bd3eaf328e0b847bf5da1e291c3
coisini_tyh <875413281@qq.com>
2022-05-27 13:59:21 +0800

    更新RemoveDuplicates.java

--- src/leetcode/RemoveDuplicates.java
+++ src/leetcode/RemoveDuplicates.java
@@ -6,14 +6,37 @@ package leetcode;
  * @Date 2022-05-26 15:43
  */
 public class RemoveDuplicates {
+    public static void main(String[] args) {
+        String s = "abbaca";//aacac
+        //StringBuffer sb = new StringBuffer(new String(s));
+        //System.out.println(sb.deleteCharAt(1));
+        System.out.println(removeDuplicates(s));
+    }
+
     public static String removeDuplicates(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                s = s.substring(0, i) + s.substring(i + 2);
-                System.out.println(s);
-                System.out.println("s = " + s);
+        StringBuffer sb = new StringBuffer(s);
+        for (int i = 0; i < sb.length() - 1; i++) {
+            if (sb.length() <= 1) return sb.toString();
+            if (sb.charAt(i) == sb.charAt(i + 1)) {
+                sb.deleteCharAt(i);
+                sb.deleteCharAt(i);
+                i = i - 2 < 0 ? -1 : i - 2;
+
             }
         }
-        return s;
+        return sb.toString();
+    }
+
+    public static String removeDuplicates1(String s) {
+        char[] S = s.toCharArray();
+        int top = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (top == -1 || S[top] != S[i]) {
+                S[++top] = S[i];
+            } else {
+                top--;
+            }
+        }
+        return String.valueOf(S, 0, top + 1);
     }
 }
--- src/qita/EachIndexOfChar.java
+++ src/qita/EachIndexOfChar.java
@@ -1,4 +1,4 @@
-package leetcode;
+package qita;
 
 import java.util.ArrayList;
 import java.util.List;
