4d0075925c4c90089e7dcf40d8331e7bd0e0295e
coisini_tyh <875413281@qq.com>
2022-05-27 09:56:58 +0800

    测试提交

--- src/leetcode/RemoveDuplicates.java
+++ src/leetcode/RemoveDuplicates.java
@@ -10,6 +10,8 @@ public class RemoveDuplicates {
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == s.charAt(i + 1)) {
                 s = s.substring(0, i) + s.substring(i + 2);
+                System.out.println("s = " + s);
+                System.out.println("s = " + s);
             }
         }
         return s;
