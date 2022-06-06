ef893da0d5f166273a38a126921805de664da440
coisini_tyh <875413281@qq.com>
2022-06-06 16:04:34 +0800

    更新leetcode题目:二分查找

---
+++ src/leetcode/Search.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * @Author 田义会
+ * @Date 2022-06-02 15:32
+ * @Description leetcode 704、二分查找
+ */
+public class Search {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] > target) {
+                right = middle;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+}
